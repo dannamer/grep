37177b9633099bf109065a724b6b4c36acb41cb2
Katelynd <rob@DESKTOP-393R8UO>
2024-01-07 19:54:45 +0500

    `

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <getopt.h>
 
-
-int main() {
-    printf();
+int main(int argc, char **argv) {
+    int opt;
+    while ((opt = getopt(argc, argv, "e:")) != -1) {
+        switch (opt) {
+            case 'e':
+                printf("Option -e with value: %s\n", optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-e value]\n", argv[0]);
+                return 1;
+        }
+    }
+    return 0;
 }
